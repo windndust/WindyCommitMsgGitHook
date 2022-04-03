20a4a2e15c68d86976a03dbdb8dc9b95a2445005
Michael Neidinger <windndust@gmail.com>
2022-04-02 19:30:40 -0500

    Upgrade to java.nio package. Word wrapping passes extensive Junit Tests
    Wraps a line to 100 characters aand injects extra characters into new line underneath.
    Lines under the wrapped line are preserved and evaluated independently for wrapping needs

--- src/main/java/org/mneidinger/windydays/githook/CommitMsgHook.java
+++ src/main/java/org/mneidinger/windydays/githook/CommitMsgHook.java
@@ -1,15 +1,16 @@
 package org.mneidinger.windydays.githook;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class CommitMsgHook {
 
@@ -17,61 +18,74 @@ public class CommitMsgHook {
 		new CommitMsgHook().processHook(args[0]);
 	}
 	
-	private boolean thirdToManyLines;
-	
 	public void processHook(String commitEditMsgFileLocation) throws IOException {
-		File commitMessageFile = new File(commitEditMsgFileLocation);
-		validateCommitMessage(commitEditMsgFileLocation);
-		if(isThirdToManyLines()) {
-			autoFormatThirdToManyLines(commitMessageFile);
+		String linesFromFile = Files.readString(Paths.get(commitEditMsgFileLocation));
+		List<String> lines = readAllLinesPreserveLineEnd(linesFromFile);
+		validateCommitMessage(lines);
+		if(lines.size()>2 && wordWrapNeeded(linesFromFile)) {
+			autoFormatThirdToManyLines(lines, Paths.get(commitEditMsgFileLocation));
 		}
 		System.exit(0);
 	}
 	
-	public void validateCommitMessage(String commitEditMsgFileLocation) throws IOException{
-		List<String> readAllLines = Files.readAllLines(Paths.get(commitEditMsgFileLocation));
+	private List<String> readAllLinesPreserveLineEnd(String linesFromFile){
+		List<String> lines = new ArrayList<String>();
+		StringBuilder line = new StringBuilder();
+		for(int i=0;i<linesFromFile.length();i++) {
+			char c = linesFromFile.charAt(i);
+			line.append(c);
+			if(c=='\n') {
+				lines.add(line.toString());
+				line.delete(0, line.length());
+			}
+		}
+		if(!line.isEmpty()) {
+			lines.add(line.toString());
+			line.delete(0, line.length());
+		}
+		return lines;
+	}
+	
+	public boolean wordWrapNeeded(String line) {
+		return line.length() > 176-line.substring(0, line.indexOf("\n")).length();
+	}
+	
+	public void validateCommitMessage(List<String> readAllLines) throws IOException{
 		String header = readAllLines.get(0);
-		System.out.println("Commit message header length: "+header.length());
-		if(header.length()>72) {
+		System.out.println("Commit message header length: "+header.strip().length());
+		if(header.strip().length()>72) {
 			System.out.println("Commit message header longer than 72 characters. Please reduce length.");
 			System.exit(1); //Exit in error resulting in blocking the commit
 		}		
-		if(readAllLines.size()>1 && !"".equals(readAllLines.get(1))) {
+		if(readAllLines.size()>1 && !System.lineSeparator().contains(readAllLines.get(1))) {
 			System.out.println("Second line of commit message is not blank. Please add blank second line to separate message header from body.");
 			System.exit(1); //Exit in error resulting in blocking the commit
 		}
 		System.out.println("Commit message passes validation");
 	}
 	
-	public boolean isThirdToManyLines() {
-		return thirdToManyLines;
-	}
-	
-	public void autoFormatThirdToManyLines(File commitMessageFile) throws FileNotFoundException {
-		Scanner readLines = new Scanner(commitMessageFile);
-
+	public void autoFormatThirdToManyLines(List<String> lines, Path path) throws IOException {
 		VisitConsumer vc = new VisitConsumer();
-		vc.addLine(readLines.nextLine());
-		vc.addLine(readLines.nextLine());
-		
-		readLines.forEachRemaining(s -> vc.accept(s));
-		vc.flush();
-		readLines.close(); 
-		
-		PrintWriter pw = new PrintWriter(commitMessageFile);
-		vc.getLinesToPrint().stream().forEach(line -> pw.println(line));
-		
-		pw.close();
+		vc.linesToPrint.add(lines.get(0));
+		vc.linesToPrint.add(lines.get(1));
+		lines.subList(2, lines.size()).forEach(line ->{
+			String[] splitLine = line.split(" ");
+			Stream.of(splitLine).forEach(word -> vc.accept(word));
+			vc.flush();
+		});
+		vc.stripLineSeparator();
+		try (BufferedWriter newBufferedWriter = Files.newBufferedWriter(path, StandardOpenOption.TRUNCATE_EXISTING)) {
+			Iterator<String> iterator = vc.linesToPrint.iterator();
+			while(iterator.hasNext()) {
+				newBufferedWriter.write(iterator.next());
+			}			
+		}
+		System.out.println("Commit message body auto formatted to 100 characters");
 	}
-	
-	private class VisitConsumer implements Consumer<String>{
 
+	private class VisitConsumer implements Consumer<String>{
 		private List<String> linesToPrint = new ArrayList<String>();
 		private StringBuilder line = new StringBuilder();
-		
-		private void addLine(String line) {
-			linesToPrint.add(line);
-		}
 		
 		@Override
 		public void accept(String word) {
@@ -82,12 +96,12 @@ public class CommitMsgHook {
 		}
 			
 		private void flush() {
-			linesToPrint.add(line.toString().strip());
+			linesToPrint.add(line.toString().strip()+System.lineSeparator());
 			line.delete(0, line.length());
 		}
 		
-		private List<String> getLinesToPrint(){
-			return linesToPrint;
+		private void stripLineSeparator() {
+			linesToPrint.set(linesToPrint.size()-1, linesToPrint.get(linesToPrint.size()-1).replace("\r\n", ""));
 		}
 	}
 }
--- src/test/java/org/mneidinger/windydays/githook/CommitMsgTest.java
+++ src/test/java/org/mneidinger/windydays/githook/CommitMsgTest.java
@@ -1,13 +1,20 @@
 package org.mneidinger.windydays.githook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
 import com.github.stefanbirkner.systemlambda.SystemLambda;
 
 public class CommitMsgTest {
@@ -24,23 +31,40 @@ public class CommitMsgTest {
 				})
 			);
 		});
-		assertEquals(0, status.get(0));
+		assertEquals(0, status.get(0), "outMessage value: "+outMessage);
 		assertEquals("Commit message header length: 29"+System.lineSeparator()+"Commit message passes validation"+System.lineSeparator(), outMessage);		
 	}
 	
 	@Test
 	public void testValidLengthHeader() throws Exception {
+		List<Integer> status = new ArrayList<>();
+		List<String> readAllLines = Files.readAllLines(Paths.get("src/test/resources/msg_header_valid_length"));
 		String outMessage = SystemLambda.tapSystemOut( () -> {
-			commitmsg.validateCommitMessage("src/test/resources/msg_header_valid_length");
+			try {
+				status.add( 
+					SystemLambda.catchSystemExit( () -> {
+						commitmsg.validateCommitMessage(readAllLines);
+					})
+				);
+			}catch(AssertionError ae) {} //on successful test run, System.exit() isn't called so catch assertionError and ignore it
 		});
+		assertEquals(0, status.size(), "System.exit() shouldn't have been called");
 		assertEquals("Commit message header length: 29"+System.lineSeparator()+"Commit message passes validation"+System.lineSeparator(), outMessage);
 	}
 	
 	@Test
 	public void testThreeLineValid() throws Exception {
+		List<Integer> status = new ArrayList<>();
 		String outMessage = SystemLambda.tapSystemOut( () -> {
-			commitmsg.validateCommitMessage("src/test/resources/msg_three_line_valid");
+			try {
+				status.add( 
+					SystemLambda.catchSystemExit( () -> {
+						commitmsg.processHook("src/test/resources/msg_three_line_valid");
+					})
+				);
+			}catch(AssertionError ae) {} // on successful test run, System.exit() isn't called so catch AssertionError and ignore it.
 		});
+		assertEquals(1, status.size(), "outMessage value: "+outMessage);
 		assertEquals("Commit message header length: 45"+System.lineSeparator()+"Commit message passes validation"+System.lineSeparator(), outMessage);
 	}
 	
@@ -50,30 +74,123 @@ public class CommitMsgTest {
 		String outMessage = SystemLambda.tapSystemOut( () -> {
 			status.add( 
 				SystemLambda.catchSystemExit( () -> {
-					commitmsg.validateCommitMessage("src/test/resources/msg_header_too_long");
+					commitmsg.processHook("src/test/resources/msg_header_too_long");
 				})
 			);
 		});
-		assertEquals(1, status.get(0));
+		assertEquals(1, status.get(0), "outMessage value: "+outMessage);
 		assertEquals("Commit message header length: 76"+System.lineSeparator()+"Commit message header longer than 72 characters. Please reduce length."+System.lineSeparator(), outMessage);
 	}
 
 	@Test
 	public void testSecondLineNotBlank() throws Exception {
+		List<String> readAllLines = Files.readAllLines(Paths.get("src/test/resources/msg_second_line_not_blank"));
 		List<Integer> status = new ArrayList<>();
 		String outMessage = SystemLambda.tapSystemOut( () -> {
 			status.add( 
 				SystemLambda.catchSystemExit( () -> {
-					commitmsg.validateCommitMessage("src/test/resources/msg_second_line_not_blank");
+					commitmsg.validateCommitMessage(readAllLines);
 				})
 			);
 		});
-		assertEquals(1, status.get(0));
+		assertEquals(1, status.get(0), "outMessage value: "+outMessage);
 		assertEquals("Commit message header length: 44"+System.lineSeparator()+"Second line of commit message is not blank. Please add blank second line to separate message header from body."+System.lineSeparator(), outMessage);
 	}
 	
 	@Test
-	public void testThirdToManyLines() throws FileNotFoundException {
-		commitmsg.autoFormatThirdToManyLines(new File("src/test/resources/msg_third_to_many_lines"));
+	public void testWrapThirdLineOnce() throws Exception {
+		Path path = Paths.get("src/test/resources/msg_third_line_118_characters");
+		List<String> readAllLines = Files.readAllLines(path);
+		List<Integer> status = new ArrayList<>();
+		String outMessage = SystemLambda.tapSystemOut( () -> {
+			status.add( 
+					SystemLambda.catchSystemExit( () -> {
+						commitmsg.processHook(path.toString());
+					})
+				);
+		});
+		assertEquals(0 , status.get(0), "outMessage value: "+outMessage);
+		assertTrue(outMessage.contains("Commit message body auto formatted to 100 characters"), "outMessage value "+outMessage);
+		List<String> readAllLines2 = Files.readAllLines(path);
+		assertEquals(4, readAllLines2.size());
+		assertEquals("over the lazy dog", readAllLines2.get(3));
+		resetTestFile(readAllLines, path);
+	}
+	
+	@Test
+	public void testWrapThirdLineTwice() throws Exception {
+		Path path = Paths.get("src/test/resources/msg_third_line_235_characters");
+		List<String> readAllLines = Files.readAllLines(path);
+		List<Integer> status = new ArrayList<>();
+		String outMessage = SystemLambda.tapSystemOut( () -> {
+			status.add( 
+					SystemLambda.catchSystemExit( () -> {
+						commitmsg.processHook(path.toString());
+					})
+				);
+		});
+		assertEquals(0 , status.get(0), "outMessage value: "+outMessage);
+		assertTrue(outMessage.contains("Commit message body auto formatted to 100 characters"), "outMessage value "+outMessage);
+		List<String> readAllLines2 = Files.readAllLines(path);
+		assertEquals(5, readAllLines2.size());
+		assertEquals("quick brown fox jumped over the lazy dog", readAllLines2.get(4));
+		resetTestFile(readAllLines, path);
+	}
+
+	@Test
+	public void testWrapThirdLineOnceFourthLine11Characters() throws Exception {
+		Path path = Paths.get("src/test/resources/msg_third_line_118_characters_fourth_line_10_characters");
+		List<String> readAllLines = Files.readAllLines(path);
+		List<Integer> status = new ArrayList<>();
+		String outMessage = SystemLambda.tapSystemOut( () -> {
+			status.add( 
+					SystemLambda.catchSystemExit( () -> {
+						commitmsg.processHook(path.toString());
+					})
+				);
+		});
+		assertEquals(0 , status.get(0), "outMessage value: "+outMessage);
+		assertTrue(outMessage.contains("Commit message body auto formatted to 100 characters"), "outMessage value "+outMessage);
+		List<String> readAllLines2 = Files.readAllLines(path);
+		assertEquals(5, readAllLines2.size());
+		assertEquals("over the lazy dog", readAllLines2.get(3));
+		assertEquals("Warm Kitty", readAllLines2.get(4));
+		resetTestFile(readAllLines, path);
+	}
+	
+	@Test
+	public void testWrapThirdAndFithLines() throws Exception {
+		Path path = Paths.get("src/test/resources/msg_third_line_235_characters_fourth_line_10_characters_fifth_line_111_characters_sixth_line_10_characters");
+		List<String> readAllLines = Files.readAllLines(path);
+		List<Integer> status = new ArrayList<>();
+		String outMessage = SystemLambda.tapSystemOut( () -> {
+			status.add( 
+					SystemLambda.catchSystemExit( () -> {
+						commitmsg.processHook(path.toString());
+					})
+				);
+		});
+		assertEquals(0 , status.get(0), "outMessage value: "+outMessage);
+		assertTrue(outMessage.contains("Commit message body auto formatted to 100 characters"), "outMessage value "+outMessage);
+		List<String> readAllLines2 = Files.readAllLines(path);
+		assertEquals(9, readAllLines2.size());
+		assertEquals("quick brown fox jumped over the lazy dog", readAllLines2.get(4));
+		assertEquals("Warm Kitty", readAllLines2.get(5));
+		assertEquals("like it also", readAllLines2.get(7));
+		assertEquals("Warm Kitty", readAllLines2.get(8));
+		resetTestFile(readAllLines, path);
+	}
+	
+	private void resetTestFile(List<String> lines, Path path) throws IOException {
+		BufferedWriter newBufferedWriter = Files.newBufferedWriter(path, StandardOpenOption.TRUNCATE_EXISTING);
+		Iterator<String> iterator =lines.iterator();
+		while(iterator.hasNext()) {
+			String line = iterator.next();
+			newBufferedWriter.write(line);
+			if(iterator.hasNext()) {
+				newBufferedWriter.newLine();
+			}
+		}
+		newBufferedWriter.close();
 	}
 }
